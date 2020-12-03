5c2b37b8e19a1b523168f01fbb0f434de70d4e10
besinci <aliosmanbesinci@gmail.com>
2020-12-03 14:39:26 +0300

    Initial commit.
    Include first challange solution.

---
+++ ConsoleApp/DS/ArrayDS.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp.DS
+{
+    public class ArrayDS
+    {
+        /// <summary>
+        /// https://www.hackerrank.com/challenges/arrays-ds/problem
+        /// </summary>
+        /// <param name="arrayInput">1 4 3 2</param>
+        /// <returns>2 3 4 1</returns>
+        public static int[] ReverseArray(int[] arrayInput)
+        {
+            int[] reversedArray = new int[arrayInput.Length];
+
+            for (int i = 0; i < arrayInput.Length; i++)
+            {
+                var indexNumber = arrayInput.Length - 1 - i;
+                reversedArray[i] = arrayInput[indexNumber];
+            }
+
+            return reversedArray;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp.DS;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var reverseArrayInput = new int[] { 1, 4, 3, 2 };
+            ArrayDS.ReverseArray(reverseArrayInput);
+
+            Console.ReadLine();
+        }
+    }
+}
