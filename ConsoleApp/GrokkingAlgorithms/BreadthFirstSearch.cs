1550fb75dc6eb2d3b4d46a28e7f8defe9bda3b82
besinci <aliosmanbesinci@gmail.com>
2021-01-17 12:48:54 +0300

    Add Breadth First Search Algorithm examples.

---
+++ ConsoleApp/GrokkingAlgorithms/BreadthFirstSearch.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp.GrokkingAlgorithms
+{
+    public class BreadthFirstSearch
+    {
+        // 6.1 Find the length of the shortest path
+        //     from start to finish.
+        // Answer: 3
+
+        // 6.2 Find the length of the shortest path
+        //     from “cab” to “bat”.
+        // Answer: 2
+
+        // 6.3 For these three lists, mark whether each one is valid or invalid.
+        // Answer: A = Invalid, B = Valid, C = Invalid
+    }
+}
